17079bd933ced28fab8af4812c260a0c74b47e65
Pureum309 <psun27@my.bcit.ca>
2023-02-16 12:29:58 -0800

    add homepage CSS

--- components/form/index.js
+++ components/form/index.js
@@ -1,4 +1,5 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Customform.module.css'
+import { Inknut_Antiqua } from '@next/font/google';
 import { useRouter } from 'next/router';
 import { useState } from 'react'
 
@@ -8,7 +9,11 @@ export default function CustomForm() {
     const [weight, setWeight] = useState('');
     const [height, setHeight] = useState('');
     const [calories, setCalories] = useState('');
-    const [submitted, setSubmitted] = useState(false); // add submitted state variable
+    const [submitted, setSubmitted] = useState(false);
+    //error msg for type
+    const [ageError, setAgeError] = useState(false);
+    const [weightError, setWeightError] = useState(false);
+    const [heightError, setHeightError] = useState(false);
 
     const r = useRouter();
 
@@ -16,78 +21,112 @@ export default function CustomForm() {
         setName(e.target.value)
     }
     const handleAgeChange = (e) => {
-        setAge(e.target.value)
+        const value = e.target.value
+        setAge(value)
+        setAgeError(value && isNaN(value) ? true : false)
     }
     const handleWeightChange = (e) => {
-        setWeight(e.target.value)
+        const value = e.target.value
+        setWeight(value)
+        setWeightError(value && isNaN(value) ? true : false)
     }
     const handleHeightChange = (e) => {
-        setHeight(e.target.value)
+        const value = e.target.value
+        setHeight(value)
+        setHeightError(value && isNaN(value) ? true : false)
     }
     const calcCalories = () => {
         setCalories(655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age));
-        setSubmitted(true); // set submitted to true after calories are calculated
+        setSubmitted(true);
     }
-
+    const handleMeals = () => {
+        r.push({
+            pathname: '/result',
+            query: {
+                name: name,
+                calories: calories,
+            }
+        })
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        calcCalories();
+        !ageError && !weightError && !heightError ? calcCalories() : null
     }
 
     return (
         <>
-            <div>
+            <div className={styles.cont} >
                 {/* need to add action in form tag */}
-                <form onSubmit={handleSubmit} className={styles.formCont}>
-                    <label>Name : </label>
-                    <input 
-                        type="text" 
-                        placeholder="Name"
-                        // required
-                        value={name}
-                        onChange={handleNameChange}
-                    />
-                    <label>Age : </label>               
-                    <input 
-                        type="number" 
-                        placeholder="Age"
-                        //required
-                        value={age}
-                        onChange={handleAgeChange}
-                    />
-                    <label>Weight(kg) : </label> 
-                    <input 
-                        type="number" 
-                        placeholder="Weight / Kilogram"
-                        //required
-                        value={weight}
-                        onChange={handleWeightChange}
-                    /> 
-                    <label>Height(cm) : </label> 
-                    <input 
-                        type="number" 
-                        placeholder="Height / Centimeter"
-                        //required
-                        value={height}
-                        onChange={handleHeightChange}
-                    />
+                <form onSubmit={handleSubmit} className={styles.form}>
+                    <div className={styles.inputCont}>
+                        <div className={styles.label}>
+                            <label>Name</label>
+                        </div>
+                        <input 
+                            className={styles.input}
+                            type="text" 
+                            placeholder="Name"
+                            required
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                    <div className={styles.inputCont}>
+                        <div className={styles.label}>
+                            <label>Age</label>
+                        </div>              
+                        <input 
+                            className={styles.input}
+                            type="text" 
+                            placeholder="Age"
+                            required
+                            value={age}
+                            onChange={handleAgeChange}
+                        />
+                        {ageError && <div className={styles.errorMessage}>Please enter the number</div>}
+                    </div>
+                    <div className={styles.inputCont}>
+                        <div className={styles.label}>
+                            <label>Weight(kg)</label>
+                        </div>
+                        <input 
+                            className={styles.input}
+                            type="text" 
+                            placeholder="Weight / Kilogram"
+                            required
+                            value={weight}
+                            onChange={handleWeightChange}
+                        /> 
+                        {weightError && <div className={styles.errorMessage}>Please enter the number</div>}
+                    </div>
+                    <div className={styles.inputCont}>
+                        <div className={styles.label}>
+                            <label>Height(cm)</label>
+                        </div> 
+                        <input 
+                            className={styles.input}
+                            type="text" 
+                            placeholder="Height / Centimeter"
+                            required
+                            value={height}
+                            onChange={handleHeightChange}
+                        />
+                        {heightError && <div className={styles.errorMessage}>Please enter the number</div>}
+                    </div>
                     {submitted && ( // conditionally render the calories and "See your meals" button
-                        <>
-                            <div>You need daily calories</div>
-                            <div>{calories}</div>
-                            <button 
-                                onClick={() => r.push({
-                                                    pathname: '/result',
-                                                    query: {
-                                                        name: name,
-                                                        calories: calories,
-                                                    }
-                                                })}>See your meals
+                        <div className={styles.result}>
+                            <div className={styles.resultBox}>
+                                <div className={styles.resultLabel}>You need daily calories</div>
+                                <div className={styles.resultValue}>{Math.ceil(calories)} kCal</div>
+                            </div>
+                            <button
+                                className={styles.button} 
+                                onClick={handleMeals}>See your daily meals
                             </button>
-                        </>
+                        </div>
                     )}
                     {!submitted && ( // conditionally render the "Submit calculate" button
-                        <button type="submit" value="Submit">Submit calculate</button>
+                        <button className={styles.button} type="submit">Calculate your calories</button>
                     )}
                 </form>
             </div>
