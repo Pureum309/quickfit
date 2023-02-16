20e4ecadc5b0243416bfff3c9a8216588bb85642
Pureum309 <psun27@my.bcit.ca>
2023-02-15 19:03:26 -0800

    creating input and result page

--- components/form/index.js
+++ components/form/index.js
@@ -1,31 +1,84 @@
+import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router';
+import { useState } from 'react'
+
 export default function CustomForm() {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [calories, setCalories] = useState(0);
+
+    const r = useRouter();
+
+    const handleNameChange = (e) => {
+        setName(e.target.value)
+    }
+    const handleAgeChange = (e) => {
+        setAge(e.target.value)
+    }
+    const handleWeightChange = (e) => {
+        setWeight(e.target.value)
+    }
+    const handleHeightChange = (e) => {
+        setHeight(e.target.value)
+    }
+    const calcCalories = () => {
+        setCalories(655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        calcCalories();
+
+        r.push({
+            pathname: '/result',
+            query: {
+                name: name,
+                calories: calories,
+            }
+        })
+    }
+
+
     return (
         <>
         <div>
             {/* need to add action in form tag */}
-            <form action="/result" method="POST">
+            <form onSubmit={handleSubmit} className={styles.formCont}>
                 <input 
                     type="text" 
                     placeholder="Name"
-                    required
+                    // required
+                    value={name}
+                    onChange={handleNameChange}
                 />
                 <input 
-                    type="text" 
+                    type="number" 
                     placeholder="Age"
-                    required
+                    //required
+                    value={age}
+                    onChange={handleAgeChange}
                 /> 
                 <input 
-                    type="text" 
+                    type="number" 
                     placeholder="Weight / Kilogram"
-                    required
+                    //required
+                    value={weight}
+                    onChange={handleWeightChange}
                 /> 
                 <input 
-                    type="text" 
+                    type="number" 
                     placeholder="Height / Centimeter"
-                    required
+                    //required
+                    value={height}
+                    onChange={handleHeightChange}
                 />
-                <button type="submit" value="Submit">Submit</button>
+                <div>You need daily calories</div>
+                <div>{calories}</div>
+                <button type="submit" value="Submit">See your meals</button>
             </form>
+            <button onClick={calcCalories}>Submit</button>
         </div>
         </>
     )
--- pages/result.js
+++ pages/result.js
@@ -1,7 +1,42 @@
+import { useRouter } from "next/router"
+import nutrition from '@/data/fastfooddata.json'
+
 export default function Result(){
+    const r = useRouter();
+    const { name, calories } = r.query
+
+    //Filter meals based on calories count
+    const filterMeals = nutrition.filter(n => n["Energy (kCal)"] <= calories);
+
+    //Random select 3 meals from the filtered list
+    const selectMeals = [];
+    while (selectMeals.length < 3 && filterMeals.length > 0) {
+        const randomIndex = Math.floor(Math.random() * filterMeals.length)
+        const meal = filterMeals[randomIndex]
+        selectMeals.push(meal);
+        filterMeals.splice(randomIndex, 1)
+    }
+
+    const totalCalories = selectMeals.reduce((acc, meal) => acc + meal["Energy (kCal)"], 0)
+
     return(
         <>
             <div>ResultPage</div>
+            <div>Hello, {name}</div>
+            <div>you need, {calories}</div>
+
+            {
+                selectMeals.map((m, index) => {
+                    return(
+                        <div key={index}>
+                            <div>{m.Company}</div>
+                            <div>{m.Product}</div>
+                            <div>{m["Energy (kCal)"]} kCal</div>
+                        </div>
+                    )
+                })
+            }
+            <div>Total Calories: {totalCalories}</div>
         </>
     )
 }
